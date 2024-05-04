a34b79338fe856b9262ca0147e5cbb11428406c2
Roma <pestovr.gpt@gmail.com>
2024-05-04 13:10:06 +0300

    fix color pallete subscriptionsPage

--- src/app/(MainApp)/subscriptions/page.tsx
+++ src/app/(MainApp)/subscriptions/page.tsx
@@ -12,7 +12,7 @@ const subscriptions = () => {
       <div className="flex flex-col gap-10 mx-8">
         <div className="textGradient mt-[40px] text-2xl">Подписки</div>
         <div className="max-w-[80vw] overflow-hidden sm-mobile:max-w-[88vw] mobile:max-w-[93vw]">
-          <div className="bg-[#f4f4f4] mb-5 flex overflow-auto scrollbar-hide relative rounded-2xl p-5 tablet-s:grid tablet-s:grid-cols-4 tablet:grid-cols-5 laptop:grid-cols-6 desktop:grid-cols-7 bd:grid-cols-9 place-items-center gap-4">
+          <div className="bg-searchText mb-5 flex overflow-auto scrollbar-hide relative rounded-2xl p-5 tablet-s:grid tablet-s:grid-cols-4 tablet:grid-cols-5 laptop:grid-cols-6 desktop:grid-cols-7 bd:grid-cols-9 place-items-center gap-4">
             <SubCard />
             <SubCard />
             <SubCard />
--- src/components/videoCard/subCard.tsx
+++ src/components/videoCard/subCard.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const SubCard = () => {
   return (
     <>
-      <div className="bg-[#ececec] rounded-xl min-w-[210px] tablet-s:min-w-0 tablet-s:max-w-[210px] w-full justify-center items-center py-4 flex flex-col gap-y-3">
+      <div className="bg-sideText rounded-xl min-w-[210px] tablet-s:min-w-0 tablet-s:max-w-[210px] w-full justify-center items-center py-4 flex flex-col gap-y-3">
         <div className="h-20 w-20 rounded-full flex justify-center items-center relative">
           <Image
             src="/images/tree.jpg"
@@ -15,11 +15,11 @@ const SubCard = () => {
         </div>
 
         <div className="flex justify-center flex-col gap-x-4">
-          <p className="text-base text-center">Lorem Ipsum</p>
-          <p className="text-xs text-center">2,3 млн подписчиков</p>
+          <p className="text-base text-center text-white">Lorem Ipsum</p>
+          <p className="text-xs text-center text-white">2,3 млн подписчиков</p>
         </div>
 
-        <button className="px-4 py-2 text-xs text-white bg-btnBlack rounded-md">
+        <button className="px-4 py-2 text-xs text-white bg-searchText rounded-md">
           Вы подписаны
         </button>
       </div>
